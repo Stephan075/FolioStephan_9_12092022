c13ed093b7af0e6df0bc80199019a712d032b948
Stephan <stephan.folioo@gmail.com>
2022-09-14 18:43:47 +0200

    update files

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -117,6 +117,7 @@ describe("Given I am connected as an employee", () => {
         })
       );
 
+      // simuler la navigation pour la tester
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
@@ -143,15 +144,28 @@ describe("Given I am connected as an employee", () => {
 
 //test d'intégration POST
 describe("Given I am a user connected as Employee", () => {
-  describe("When I am on NewBill Page and submit the form", () => {
-    test("then it posts to API and fails with 400 message error on Bills page", async () => {
+  describe("When I am on NewBill Page and submit the for and an error occurs on API", () => {
+    beforeEach(() => {
       jest.spyOn(mockStore, "bills");
       jest.spyOn(console, "error").mockImplementation(() => {});
 
-      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
-      document.body.innerHTML = `<div id="root"></div>`;
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Admin",
+          email: "a@a",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.appendChild(root);
       router();
-
+    });
+    test("then it posts to API and fails with 404 message error on Bills page", async () => {
+      // simuler la navigation pour la tester
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
@@ -159,10 +173,10 @@ describe("Given I am a user connected as Employee", () => {
       mockStore.bills = jest.fn().mockImplementation(() => {
         return {
           update: () => {
-            return Promise.reject(new Error("Erreur 400"));
+            return Promise.reject(new Error("Erreur 404"));
           },
           list: () => {
-            return Promise.reject(new Error("Erreur 400"));
+            return Promise.reject(new Error("Erreur 404"));
           },
         };
       });
